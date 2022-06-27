21b8fc927754abca02f9cca760db304aebd6bff8
Gabriel Friederichs <gabriel_friederichs@yaskawa.com>
2022-06-27 16:37:12 -0500

    Environment Setup

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+const port = 5000;
+
+// Server Setup
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.set("view engine", "ejs");
+
+app.listen(port, function() {
+  console.log("Server started on port " + port);
+})
+
+// '/'
+// GET: View page
+app.route("/")
+  .get(function(req, res) {
+    res.render("home");
+  });
+
+// '/'
+// GET: View page
+
+app.route("/register")
+  .get(function(req, res) {
+    res.render("register");
+  });
+
+// '/'
+// GET: View page
+
+app.route("/login")
+  .get(function(req, res) {
+    res.render("login");
+  });
